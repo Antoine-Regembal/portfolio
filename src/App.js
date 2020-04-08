4ac460b2dc7d3a13d9a89732fa3a9d8aedb69a48
Antoine Regembal <antoine.regembal@hotmail.fr>
2020-04-08 20:26:08 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import "./App.css";
+import AppRouter from "./App.router";
+
+const App = () => <AppRouter />;
+
+export default App;
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./Home.css";
+
+const Home = () => <div className="home">Home</div>;
+
+export default Home;
