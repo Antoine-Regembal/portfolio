c9bbe44367a9c1d2685c5fa8e17ac1ee87c4c59a
Antoine Regembal <antoine.regembal@hotmail.fr>
2020-04-09 19:51:44 +0200

    Translation english french

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -3,8 +3,8 @@ import React from "react";
 import "./Portfolio.css";
 import Home from "../Home";
 import Presentation from "../Presentation";
-import Technologies from "../Technologies";
 import Cursus from "../Cursus";
+import Technologies from "../Technologies";
 import AppFooter from "../AppFooter";
 import Github from "../Github";
 import Navigation from "../Navigation";
@@ -21,7 +21,6 @@ const Portfolio = ({ appContent, appCommon, setLanguage }) => (
       presentationContent={appContent.presentation}
       name={appCommon.name}
     />
-    <Technologies technologiesContent={appContent.technologies} />
     <Cursus cursusContent={appContent.cursus} />
     <AppFooter />
     <Github githubContent={appCommon.github} />
